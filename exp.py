a6c028185b0b7a797fcca7058807cad24b6ec240
sabaszx <5910110573@psu.ac.th>
2019-10-17 01:13:28 +0700

    asda

--- exp.py
+++ exp.py
@@ -172,7 +172,7 @@ def main():
              
             #final result
             Result  = input           
-            output.write(Result)
+            output.write(Result + "\n")
            
 
         except EOFError:
