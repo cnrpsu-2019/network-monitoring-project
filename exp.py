34c231241ceb667ba4dca5295eff105cf31563ad
sabaszx <5910110573@psu.ac.th>
2019-10-16 20:32:56 +0700

    asda

--- exp.py
+++ exp.py
@@ -125,7 +125,7 @@ def main():
             cLWlanChdEnable = modApname0.replace("cLWlanChdEnable.1", "Wlan channel enable:")
             cLApDot11IfSlotId2 = cLWlanChdEnable.replace("cLApDot11IfSlotId.0","AP interface slot ID:")
             
-            convertsingle = cLApDot11IfSlotId2.replace("'","0")
+            convertsingle = cLApDot11IfSlotId2.replace("'","")
             #final result
             Result  = convertsingle
             
