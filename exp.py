f2d8cdf91f746cf0cb46a15d661b31a201abc011
sabaszx <5910110573@psu.ac.th>
2019-10-16 20:01:21 +0700

    asd

--- exp.py
+++ exp.py
@@ -123,6 +123,7 @@ def main():
             modApname0 = cLLastDetectingRadioMACAddress.replace("AP Name:0", "AP name:")
 
             cLWlanChdEnable = modApname0.replace("cLWlanChdEnable.1", "Wlan channel enable:")
+            cLApDot11IfSlotId2 = cLWlanChdEnable.replace("cLApDot11IfSlotId.0","AP slot ID:")
             
             #final result
             Result  = cLWlanChdEnable
