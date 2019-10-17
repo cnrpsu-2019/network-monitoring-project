33437ad36425adbc4c98e3044824f482c0dd4e8e
sabaszx <5910110573@psu.ac.th>
2019-10-17 20:23:31 +0700

    asdasd

--- exp.py
+++ exp.py
@@ -15,7 +15,7 @@ def main():
             input = raw_input()
             filtered = input.replace("<UNKNOWN>","")
             wrongtypeRemove = filtered.replace("Wrong Type (should be Gauge32 or Unsigned32)","")
-
+            ''.join(e for e in wrongtypeRemove if e.isalnum())
 
             #final result
             Result  = wrongtypeRemove           
