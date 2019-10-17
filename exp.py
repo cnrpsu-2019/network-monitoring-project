080e79b5fb72e2f05b142a6a0bf1413393c2eb60
sabaszx <5910110573@psu.ac.th>
2019-10-17 20:51:06 +0700

    asd

--- exp.py
+++ exp.py
@@ -15,7 +15,7 @@ def main():
             input = raw_input()
             filtered = input.replace("<UNKNOWN>","")
             wrongtypeRemove = filtered.replace("Wrong Type (should be Gauge32 or Unsigned32)","")
-            screen = re.sub('[^A-Za-z0-9]+[.]+[:]', '', wrongtypeRemove)
+            screen = re.sub('[^A-Za-z0-9].*', '', wrongtypeRemove)
 
             #final result
             Result  = screen           
