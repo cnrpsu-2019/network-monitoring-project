8a0f3e2abe8fac7eefc754fb8b6a8585afb1bf35
sabaszx <5910110573@psu.ac.th>
2019-10-18 18:01:50 +0700

    sdfdf

--- exp.py
+++ exp.py
@@ -45,7 +45,7 @@ def main():
 
             #outstr
             outstr  = weirdRemove.translate(None, bad_chars)
-            pattern = "^'...'$"
+            pattern = "^''$"
             result = re.sub(pattern, '', outstr)
             
             output.write(result + '\n')
