795d2ceb35f71a9313bb89f2eafc92b5ad6e9eb2
sabaszx <5910110573@psu.ac.th>
2019-10-18 17:37:23 +0700

    ads

--- exp.py
+++ exp.py
@@ -28,15 +28,17 @@ def main():
             showDate = filtered.replace("UDP: 172.30.232.2:32768-172.30.232.250:162", strnow)
             
             dot0replace = showDate.replace(".0 ", ' ')
-            wrongtypeRemove = dot0replace.replace("Wrong Type (should be Gauge32 or Unsigned32)","" )
+            wronglist = ['Wrong Type (should be Gauge32 or Unsigned32)', 'Wrong Type should be Timeticks:']
+
+            wrongtypeRemove = replaceMultiple(dot0replace, wronglist, '')
             timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
-            mibList = ['CISCO-LWAPP-ROGUE-MIB::','AIRESPACE-WIRELESS-MIB::','CISCO-LWAPP-DOT11-CLIENT-MIB::','CISCO-LWAPP-AP-MIB::','CISCO-LWAPP-AP-MIB::']
+            mibList = ['CISCO-LWAPP-RRM-MIB::','CISCO-LWAPP-ROGUE-MIB::','AIRESPACE-WIRELESS-MIB::','CISCO-LWAPP-DOT11-CLIENT-MIB::','CISCO-LWAPP-AP-MIB::','CISCO-LWAPP-AP-MIB::']
             hideMIB = replaceMultiple(timestamp, mibList, '')
             event = hideMIB.replace("SNMPv2-MIB::snmpTrapOID ","Event : ")
 
             # weirdList = ['.hx.F','..E.','.Tb','. ','.98y','.p.','.hx ','..o1','.XVM.','b.5.','.T','.Hn','.L','...0r','.A.','..A.','..0', '.gx.','.hx','.X','.i.','.W.g','.W','...j','.c ','.L','.    . ','.LkY ','... ','". ','"','..0','.p. ','.. ','. ','.M.',' : ','.c.q.','..p','..U','.hx','.pU.I','.H.0','v.','.. ','.j.','. ','.  ','..EB.M','Z','J','Q','..e.','..A.','..9.','.l.1.Fb','.p.','..x','..L.','.z.','..f','..N..j.','.i.', '.N.','.K','.h.','......','...','..','hx','wt','.o.']
             # weirdRemove = replaceMultiple(hideMIB, weirdList, '')
-            weirdList = ['. ']
+            weirdList = ['. ', '.1 ']
             weirdRemove = replaceMultiple(event, weirdList, '')
             bad_chars = "/\\!$^&*|({)[}>_<]~+=#$%;`@?"
             #rgx = re.compile('[%s]' % bad_chars)
