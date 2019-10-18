a7475613a86ca3e5880fdaaa7080bb90d5cf1b8c
sabaszx <5910110573@psu.ac.th>
2019-10-18 17:10:13 +0700

    asda

--- exp.py
+++ exp.py
@@ -26,8 +26,8 @@ def main():
             input = raw_input()
             filtered = input.replace("<UNKNOWN>","")
             dot0replace = filtered.replace(".0 ", ' ')
-            wrongtypeRemove = dot0replace.replace("Wrong Type (should be Gauge32 or Unsigned32)","")
-            timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::UDP: 172.30.232.2:32768-172.30.232.250:162",strnow)
+            wrongtypeRemove = dot0replace.replace("Wrong Type (should be Gauge32 or Unsigned32)","" + '\n')
+            timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::UDP: 172.30.232.2:32768-172.30.232.250:162",strnow + '\n')
             mibList = ['CISCO-LWAPP-ROGUE-MIB::','AIRESPACE-WIRELESS-MIB::','CISCO-LWAPP-DOT11-CLIENT-MIB::','CISCO-LWAPP-AP-MIB::','CISCO-LWAPP-AP-MIB::']
             hideMIB = replaceMultiple(timestamp, mibList, '' + '\n')
 
