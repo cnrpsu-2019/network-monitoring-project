dfb1384ec0bdbb9d94236e53cd751c0ee8e61e3c
sabaszx <5910110573@psu.ac.th>
2019-10-18 19:33:53 +0700

    qwewr

--- exp.py
+++ exp.py
@@ -31,9 +31,9 @@ def main():
 
             wrongtypeRemove = replaceMultiple(showDate, wronglist, '')
             timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
-            mibList = ['CISCO-LWAPP-SI-MIB::','CISCO-LWAPP-RRM-MIB::','CISCO-LWAPP-ROGUE-MIB::','AIRESPACE-WIRELESS-MIB::','CISCO-LWAPP-DOT11-CLIENT-MIB::','CISCO-LWAPP-AP-MIB::','CISCO-LWAPP-AP-MIB::']
+            mibList = ['SNMPv2-MIB::','CISCO-LWAPP-SI-MIB::','CISCO-LWAPP-RRM-MIB::','CISCO-LWAPP-ROGUE-MIB::','AIRESPACE-WIRELESS-MIB::','CISCO-LWAPP-DOT11-CLIENT-MIB::','CISCO-LWAPP-AP-MIB::','CISCO-LWAPP-AP-MIB::']
             hideMIB = replaceMultiple(timestamp, mibList, '')
-            event = hideMIB.replace("SNMPv2-MIB::snmpTrapOID ","Event : ")
+            event = hideMIB.replace("snmpTrapOID","Event : ")
 
             # weirdList = ['.hx.F','..E.','.Tb','. ','.98y','.p.','.hx ','..o1','.XVM.','b.5.','.T','.Hn','.L','...0r','.A.','..A.','..0', '.gx.','.hx','.X','.i.','.W.g','.W','...j','.c ','.L','.    . ','.LkY ','... ','". ','"','..0','.p. ','.. ','. ','.M.',' : ','.c.q.','..p','..U','.hx','.pU.I','.H.0','v.','.. ','.j.','. ','.  ','..EB.M','Z','J','Q','..e.','..A.','..9.','.l.1.Fb','.p.','..x','..L.','.z.','..f','..N..j.','.i.', '.N.','.K','.h.','......','...','..','hx','wt','.o.']
             # weirdRemove = replaceMultiple(hideMIB, weirdList, '')
