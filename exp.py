8ad82186d0e14a185dbf853a6fc7f58310a10a9e
sabaszx <5910110573@psu.ac.th>
2019-10-19 15:14:20 +0700

    asdasd

--- exp.py
+++ exp.py
@@ -62,7 +62,7 @@ def main():
             ,'..A..v.','....i..','.:','...i..','.h..1:S','....4.','..o...','..mR...','.Lk....','..N..N.','.,a....','.....-.','..o1..'
             ,'.hT....','......    .','..k.7.9','.0.r.','CX..','.Z.','..z..u','..Ag','.Tb..q','.t.OxE.','..YqWR','.t..c.w','..YqWR','..UX'
             ,'.t..c.w','..PF..','-.','.l..oT','.xaZ..','.vE.','.8-.','.Tb..G','.Q.','.V5','t..','..','.0.','..S','V.Oj','.vE.','.z..','.pv.'
-            ,'QY.','.N6.3.','7.N','.zO.','..PF..','.Df..z.']
+            ,'QY.','.N6.3.','7.N','.zO.','..PF..','.Df..z.','f.f','6.N','.4Bb','B.HT','o.l','g.','.D.M']
             outstr  = weirdRemove.translate(None, bad_chars)
             result = replaceMultiple(outstr,bad_list,'')
 
