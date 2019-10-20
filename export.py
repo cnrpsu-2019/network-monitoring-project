485fd04e859ef653c337a9539550fc5ebf99c06a
sabaszx <5910110573@psu.ac.th>
2019-10-20 16:32:18 +0700

    wqe

--- export.py
+++ export.py
@@ -2,20 +2,15 @@ import datetime
 import json
 
 def main():
-    running = True
+    #running = True
     now = datetime.datetime.now()
     #strnow = now.strftime("%X") #current time
     #log file date
     fileDate = now.strftime("%d-%b-%Y")
     fileName = "trapd-" + fileDate + ".log"
     read = open('/home/bass/trap-receiver/' + fileName, 'r')
-
-    while running:
-        try:
-            print(read.readline)
-        except EOFError:
-            running = False
-    read.close()
+    read.readline()
+    
 if __name__ == '__main__':
     main()
 
