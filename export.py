294d51fcbeb07b7a61cc958fc0ecc9820483dc84
sabaszx <5910110573@psu.ac.th>
2019-10-20 16:35:56 +0700

    asdasd

--- export.py
+++ export.py
@@ -1,16 +1,6 @@
 import datetime
-import json
-
-def main():
-    #running = True
-    now = datetime.datetime.now()
-    #strnow = now.strftime("%X") #current time
-    #log file date
-    fileDate = now.strftime("%d-%b-%Y")
-    fileName = "trapd-" + fileDate + ".log"
-    read = open('/home/bass/trap-receiver/' + fileName, 'r')
-    read.readline()
-    
-if __name__ == '__main__':
-    main()
-
+now = datetime.datetime.now()
+fileDate = now.strftime("%d-%b-%Y")
+fileName = "trapd-" + fileDate + ".log"
+output = open('/home/bass/trap-receiver/' + fileName, 'r')
+output.readline()
