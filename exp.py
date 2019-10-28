349aa879317ba2070383d52a04ad3f060396b878
sabaszx <5910110573@psu.ac.th>
2019-10-28 23:39:51 +0700

    asd

--- exp.py
+++ exp.py
@@ -40,7 +40,7 @@ def main():
     
     while running:
         try:
-            input = input()
+            input = raw_input()
             filtered = input.replace("<UNKNOWN>","" )
             showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
         
