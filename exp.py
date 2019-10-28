8b2a62b2aeaa3f8daa0a3b7157893b59ce6d3ac2
sabaszx <5910110573@psu.ac.th>
2019-10-28 23:38:45 +0700

    ZX

--- exp.py
+++ exp.py
@@ -40,8 +40,8 @@ def main():
     
     while running:
         try:
-            read_input = raw_input()
-            filtered = read_input.replace("<UNKNOWN>","" )
+            input = input()
+            filtered = input.replace("<UNKNOWN>","" )
             showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
         
             wronglist = ['Wrong Type (should be Gauge32 or Unsigned32)', 'Wrong Type should be Timeticks:','Wrong Type should be OCTET STRING:'
