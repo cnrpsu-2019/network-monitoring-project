f7ef2e02a5508feced1c6e2343642624b449e1c9
sabaszx <5910110573@psu.ac.th>
2019-10-30 06:09:27 +0700

    test db

--- sendtoDB.py
+++ sendtoDB.py
@@ -1,1 +1,49 @@
 import influxdb
+import datetime
+import re
+import string
+# import yaml
+import sys
+import Filterx
+
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces :
+        # Check if string is in the main string
+        if elem in mainString :
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    
+    return  mainString     
+
+def main():
+    running = True
+    now = datetime.datetime.now()
+    strnow = now.strftime("%X") #current time
+    #log file date
+    fileDate = now.strftime("%d-%b-%Y")
+    fileName = "trapd-" + fileDate + ".log"
+    readFile = open('/home/bass/trap-receiver/' + fileName, 'r')
+    
+    while running:
+        try:
+            readInput = readFile.read()
+            filtered = readInput.replace("<UNKNOWN>","" )
+            showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
+            wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
+            timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
+            hideMIB = replaceMultiple(timestamp, Filterx.mibList, '')
+            event = hideMIB.replace("snmpTrapOID","Event")
+            prefixRemove = replaceMultiple(event, Filterx.prefixList, '')
+            weirdRemove = replaceMultiple(prefixRemove, Filterx.weirdList, ' ')
+            bad_chars = "/\\!$^&*|'({)[}>_<]~+=#$%;`@?"
+            #outstr
+            outstr  = weirdRemove.translate(None, bad_chars)
+            result = replaceMultiple(outstr,Filterx.bad_list,' ')
+           
+            print(result + '\n')
+        except EOFError:
+            running = False
+    readInput.close()
+if __name__ == '__main__':
+    main()
