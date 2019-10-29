f12df1b9a7cf5a5faf531c31f68c952c7c9a2047
sabaszx <5910110573@psu.ac.th>
2019-10-29 20:11:32 +0700

    adsf

--- exp.py
+++ exp.py
@@ -26,8 +26,8 @@ def main():
     
     while running:
         try:
-            input = input()
-            filtered = input.replace("<UNKNOWN>","" )
+            read = input()
+            filtered = read.replace("<UNKNOWN>","" )
             showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
         
             wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
