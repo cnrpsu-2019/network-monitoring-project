5651f0806888b727f9ea21757b4d50cb56fd28f1
sabaszx <5910110573@psu.ac.th>
2019-11-01 15:49:59 +0700

    zxczx

--- exp.py
+++ exp.py
@@ -63,9 +63,9 @@ def main():
             result = replaceMultiple(outstr,Filterx.bad_list,' ')
             #lines = sys.stdin.readlines(result)
 
-            client = InfluxDBClient('localhost',8086,'sabaszx','admin','snmptrapd')
+            #client = InfluxDBClient('localhost',8086,'sabaszx','admin','snmptrapd')
 
-            trap['oid'] = None
+            trap['ApName'] = None
             trap['UptimeInstance'] = None
             trap['varbinds'] = []
             trap['varbinds_dict'] = {}
@@ -78,7 +78,7 @@ def main():
                     if "snmpTrapOID" in line:
                         varbind = line.strip().split(" ", 1)
                         trap['varbinds_dict'][varbind[0]] = varbind[1]
-                        trap['oid'] = varbind[1].strip()
+                        trap['ApName'] = varbind[1].strip()
                         continue
                 trap['varbinds'].append(line.strip().replace(" ", "="))
                 varbind = line.strip().split(" ", 1)
@@ -92,11 +92,11 @@ def main():
                 if config['all'].get('measurement', None) is not None:
                     if config['all'].get('permit', None) is not None:
                         for rule in config['all']['permit']:
-                            if rule in trap['oid']:
+                            if rule in trap['ApName']:
                                 datapoints.append(get_all_traps_influx_datapoint(config, trap))
                     elif config['all'].get('deny', None) is not None:
                         for rule in config['all']['deny']:
-                            if rule in trap['oid']:
+                            if rule in trap['ApName']:
                                 break
                         else:
                             # if deny rule is not match
@@ -106,30 +106,12 @@ def main():
                         datapoints.append(get_all_traps_influx_datapoint(config, trap))
             
 
-            # processing for each type of traps according to the mappings configuration
-            cfg_mappings = config.get('mappings', None)
-            if cfg_mappings is not None:
-                mapping = cfg_mappings.get(trap['oid'], None)
-                if mapping is not None:
-                    oid_datapoint = {}
-                    oid_datapoint['measurement'] = mapping['measurement']
-                    oid_datapoint['tags'] = {}
-                    oid_datapoint['tags'].update({ config['all']['tags'].get('host_dns', 'host_dns'): trap['host_dns'] })
-                    oid_datapoint['tags'].update({ config['all']['tags'].get('host_ip', 'host_ip'): trap['host_ip'] })
-                    oid_datapoint['fields'] = {}
-                    for varbind in trap['varbinds_dict'].keys():
-                        for element in mapping['tags']:
-                            if element in varbind:
-                                oid_datapoint['tags'].update({ element : trap['varbinds_dict'][varbind] })
-                        for element in mapping['fields']:
-                            if element in varbind:
-                                oid_datapoint['fields'].update({ element: trap['varbinds_dict'][varbind] })
-                
+            
             # export to influxdb
             if datapoints != [] and config.get('influxdb', None) is not None:
                 dbclients = []
                 for server in config['influxdb'].get('server', []):
-                    dbclient = InfluxDBClient(host=server['ip'], port=server['port'], username=server['user'], password=server['pass'], database=server['db'])
+                    dbclient = InfluxDBClient(host=server['172.30.232.250'], port=server['8086'], username=server['sabaszx'], password=server['admin'], database=server['snmptrapd'])
                     dbclients.append(dbclient)
                 if dbclients != []:
                     for dbclient in dbclients:
