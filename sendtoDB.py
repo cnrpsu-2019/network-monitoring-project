5c948df9346e983a64e1b767bb314cd53baad528
sabaszx <5910110573@psu.ac.th>
2019-11-02 16:31:20 +0700

    add dict test

---
+++ sendtoDB.py
@@ -0,0 +1,89 @@
+StationDeauthenticate = {
+    "StationAPMacAddr": "",
+    "StationAPIfSlotId": "",
+    "StationReasonCode": "",
+    "UserIpAddress": "",
+    "StationUserName": "",
+    "StationMacAddress": "",
+    "APName": ""
+}
+
+StationDisassociate = {
+    "StationAPMacAddr": "",
+    "StationAPIfSlotId": "",
+    "StationReasonCode": "",
+    "UserIpAddress": "",
+    "StationUserName": "",
+    "StationMacAddress": "",
+    "APName": ""
+}
+
+StationAsssociate = {
+    "StationAPMacAddr": "",
+    "StationAPIfSlotId": "",
+    "UserIpAddress": "",
+    "StationUserName": "",
+    "StationMacAddress": "",
+    "APName": ""
+}
+
+ClientMovedToRunState = {
+    "ClientMacAddress": "",
+    "ApName": "",
+    "ApMacAddress": "",
+    "ApIfSlotId": "",
+    "ClientIPAddress": "",
+    "ClientUsername": "",
+    "ClientSSID": ""
+}
+
+ClientSessionTrap = {
+    "ApIfSlotId": "",
+    "ApName": "",
+    "ClientByIpAddressType": "",
+    "ClientByIpAddress": "",
+    "ClientUsername": "",
+    "ClientSSID": "",
+    "ClientSessionID": "",
+    "ApMacAddress": ""
+}
+
+ApRogueDetected = {
+    "ApSysMacAddress": "",
+    "ApName": "",
+    "ApRogueApMacAddress": "",
+    "ApEthernetIfSlotId": "",
+    "ApIfType": "",
+    "ApRogueDetectedChannel": "",
+    "ApRSSI": "",
+    "ApSNR": "",
+    "ApRogueAPOnWiredNetwork": "",
+    "ApAdhocRogue": "",
+    "ApRogueApSsid": "",
+    "ApRogueClassType": "",
+    "ApRogueMode": "",
+    "ApRogueIsClassifiedByRule": "",
+    "ApSeverityScore": "",
+    "ApRuleName": "",
+    "ApRogueClassifiedApMacAddress": "",
+    "ApRogueClassifiedRSSI": ""
+}
+
+RogueClientDetected = {
+    "ClientMacAddress": "",
+    "LastDetectingRadioMACAddress": "",
+    "ApRogueApMacAddress": "",
+    "ApRogueMode": "",
+    "RogueClientTotalDetectingAPs": "",
+    "RogueClientFirstReported": "",
+    "RogueClientLastReported": "",
+    "RogueClientGatewayMac": ""
+}
+
+RogueAPRemoved = {
+    "RogueAPMacAddress": "",
+    "RogueAPMacAddress": "",
+    "RogueAPSlotId": "",
+    "RogueAPRadioType": "",
+    "RogueAPName": ""
+}
