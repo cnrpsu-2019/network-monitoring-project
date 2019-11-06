b7fb1c603297f6cabd1e23b295d7ff8dff8b0502
sabaszx <sabaszx@5910110573.psu.ac.th>
2019-11-06 23:20:50 +0700

    asdasd

--- exp.py
+++ exp.py
@@ -25,7 +25,7 @@ def main():
  
     while running:
         try:
-            read = sys.stdin.read()
+            read = raw_input()
             filtered = read.replace("<UNKNOWN>","" )
             showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
         
