1ae390cd0f87b13e5c5a7aed748326bc1d95520d
sabaszx <5910110573@psu.ac.th>
2019-11-07 01:07:34 +0700

    asdasd

--- sendtoDB.py
+++ sendtoDB.py
@@ -4,16 +4,16 @@ import datetime
 import exp
 
 def get_all_traps_influx_datapoint(config, trap):
-    varbinds = ", ".join(trap['varbinds'])
+    varbinds = ", ".join(trap['description'])
     datapoint = {
         "measurement" : config['all']['measurement'],
         "tags": {
-            config['all']['tags'].get('host_dns', 'host_dns'): trap['host_dns'],
-            config['all']['tags'].get('host_ip', 'host_ip'): trap['host_ip'],
-            config['all']['tags'].get('oid', 'oid'): trap['oid']
+            config['all']['tags'].get('ap', 'ap'): trap['ap'],
+            config['all']['tags'].get('event', 'event'): trap['event'],
+            config['all']['tags'].get('mac_addr', 'mac_addr'): trap['mac_addr']
         },
         "fields" : {
-            "varbinds" : varbinds
+            "description" : varbinds
         }
     }
     return datapoint
@@ -27,3 +27,24 @@ fileName = "trapd-" + fileDate + ".log"
 read = open('/home/bass/receive/' + fileName, 'r+')
 
 line = read.readlines()
+
+# parsing SNMP stuff
+trap = {}
+trap['mac_addr'] = None
+trap['sysuptime'] = None
+trap['description'] = []
+trap['description_dict'] = {}
+for line in line[2:]:
+    if trap['sysuptime'] is None:
+        if "sysUpTime" in line:
+            trap['sysuptime'] = line.split(" ")[1].strip()
+            continue
+    if trap['description'] is None:
+        if "snmpTrapOID" in line:
+            varbind = line.strip().split(" ", 1)
+            trap['description_dict'][varbind[0]] = varbind[1]
+            trap['description'] = varbind[1].strip()
+            continue
+    trap['description'].append(line.strip().replace(" ", "="))
+    varbind = line.strip().split(" ", 1)
+    trap['description'][varbind[0]] = varbind[1]
