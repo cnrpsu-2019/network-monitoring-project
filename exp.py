91bda7c0af6717d244df0a8523ffbe8d1de0fcf7
sabaszx <5910110573@psu.ac.th>
2019-11-06 09:59:17 +0700

    asdasd

--- exp.py
+++ exp.py
@@ -1,54 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-<<<<<<< HEAD
-import Filterx
-
-def replaceMultiple(mainString, toBeReplaces, newString):
-    # Iterate over the strings to be replaced
-    for elem in toBeReplaces :
-        # Check if string is in the main string
-        if elem in mainString :
-            # Replace the string
-            mainString = mainString.replace(elem, newString)
-    
-    return  mainString
-
-def main():
-    running = True
-    now = datetime.datetime.now()
-    strnow = now.strftime("%X") #current time
-    #log file date
-    fileDate = now.strftime("%d-%b-%Y")
-    fileName = "trapd-" + fileDate + ".log"
-    output = open('/home/bass/receive/' + fileName, 'a')
- 
-    while running:
-        try:
-            read = raw_input()
-            filtered = read.replace("<UNKNOWN>","" )
-            showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
-        
-            wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
-            timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
-            hideMIB = replaceMultiple(timestamp, Filterx.mibList, '')
-            event = hideMIB.replace("snmpTrapOID","Event")
-            prefixRemove = replaceMultiple(event, Filterx.prefixList, '')
-            weirdRemove = replaceMultiple(prefixRemove, Filterx.weirdList, ' ')
-            bad_chars = "/\\!$^&*|'({)[}>_<]~+=#$%;`@?"
-            #outstr - write log files into local server
-            outstr  = weirdRemove.translate(None, bad_chars)
-            sameAp = outstr.replace("ApName", "APName")
-            result = replaceMultiple(sameAp,Filterx.bad_list,' ')
-            output.write(result+ '\n')
-            
-           
-        except EOFError:
-            running = False
-    output.close()
-if __name__ == '__main__':
-    main()
-=======
 import logging
 import logging.handlers
 import yaml
@@ -196,4 +148,3 @@ if datapoints != [] and config.get('influxdb', None) is not None:
         for dbclient in dbclients:
             dbclient.write_points(datapoints)
 
->>>>>>> 1b7698fc5d7a870e77fd46de001f741bbfac9aa7
