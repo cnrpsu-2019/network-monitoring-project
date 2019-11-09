125d2507936ff3de436715aff4c1ac837da8e37c
sabaszx <5910110573@psu.ac.th>
2019-11-09 21:53:04 +0700

    recover

---
+++ exp.py
@@ -0,0 +1,63 @@
+import datetime
+import re
+import string
+import sys
+import Filterx
+import sendtoDB
+
+from influxdb import InfluxDBClient
+
+
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces :
+        # Check if string is in the main string
+        if elem in mainString :
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    
+    return  mainString
+
+def main():
+    running = True
+    now = datetime.datetime.now()
+    strnow = now.strftime("%X") #current time
+    #log file date
+    fileDate = now.strftime("%d-%b-%Y")
+    fileName = "trapd-" + fileDate + ".log"
+    output = open('/home/bass/receive/' + fileName, 'a')
+    #read same data
+    toReadData = open('/home/bass/receive/' + fileName, 'r')
+
+    client = InfluxDBClient('localhost',8086,'sabaszx','admin','snmptrapd')
+    client.switch_database('snmptrapd')
+
+    
+   
+    while running:
+        try:
+            input = raw_input()
+            filtered = input.replace("<UNKNOWN>","" )
+            showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
+        
+            wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
+            timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
+            hideMIB = replaceMultiple(timestamp, Filterx.mibList, '')
+            event = hideMIB.replace("snmpTrapOID","Event")
+            prefixRemove = replaceMultiple(event, Filterx.prefixList, '')
+            weirdRemove = replaceMultiple(prefixRemove, Filterx.weirdList, ' ')
+            bad_chars = "/\\!$^&*|'({)[}>_<]~+=#$%;`@?"
+            #outstr - write log files into local server
+            outstr  = weirdRemove.translate(None, bad_chars)
+            result = replaceMultiple(outstr,Filterx.bad_list,' ')
+            output.write(result + '\n')
+            
+            print(toReadData.read())
+
+                        
+        except EOFError:
+            running = False
+    output.close()
+    toReadData.close()
+if __name__ == '__main__':
+    main()
