0b262acdbd8b4bacdf27e9b225509a5396d31bad
sabaszx <5910110573@psu.ac.th>
2020-03-04 22:45:38 +0700

    talk with rubber duck

--- exp.py
+++ exp.py
@@ -19,15 +19,15 @@ def replaceMultiple(mainString, toBeReplaces, newString):
     return  mainString
 
 def main():
-    running = True
     now = datetime.datetime.now()
     strnow = now.strftime("%X") #current time
     #log file date
     fileDate = now.strftime("%d-%b-%Y")
     fileName = "trapd-" + fileDate + ".log"
-    output = open('/home/bass/receive/' + fileName, 'a')
-    readfile = open('/home/bass/receive/' + fileName, 'r')
-    while running:
+    output = open('/home/bass/receive/' + fileName, 'a') #write into local server
+    readfile = open('/home/bass/receive/' + fileName, 'r') #read from local server that we wrote
+    while True:
+
         try:
             input_raw = input()
             #filter weird string sction
@@ -47,25 +47,27 @@ def main():
             #outstr - write log files into local server
             result = replaceMultiple(outstr,Filterx.bad_list,' ')
 
-            #write to local
+            #write to local serever
             output.write(result + '\n')
-            #export to db
+
+            #read files
             line = readfile.read()
             if not line:
                 time.sleep(1)
+
             #associate users
-            PatternAssociate = ['Associate', 'Sessiontrap''MovedToRunState','Username']
+            PatternAssociate = ['Associate', 'Sessiontrap','MovedToRunState','Username']
             for patterns in PatternAssociate:
                 if re.search(patterns, line):
                     JadeBowx.countUserAssociate()
                     print('associated')
-                    time.sleep(1)
+                    
 
             PatternDeauth = ['StationDeauthenticate', 'Disassociate','Deauthenticate']
             for patterns in PatternDeauth:
                 if re.search(patterns, line):
                     JadeBowx.countUserDauth()
-                    time.sleep(1)
+                     #sleep for 5 secs
 
             # ap each floor list
             floor_01_list = ['AP3-46-R010-146','AP218-FL01-E','AP217-FL01-W','AP204-R100','AP216-R101','AP211-Shop']
@@ -78,164 +80,162 @@ def main():
                 if re.search(patterns, line):
                     JadeBowx.countFloor01()
                     print('floor1')
-                    time.sleep(1)
+                    
 
             for patterns in floor_02_list:
                 if re.search(patterns, line):
                     JadeBowx.countFloor02()
                     print('floor2')
-                    time.sleep(1)
+                    
                     
             for patterns in floor_03_list:
                 if re.search(patterns, line):
                     JadeBowx.countFloor03()
                     print('floor3')
-                    time.sleep(1)
+                    
 
             for patterns in floor_04_list:
                 if re.search(patterns,line):
                     JadeBowx.countFloor04()
                     print('floor4')
-                    time.sleep(1)
+                    
                         
             if 'CoEWiFi' in line:
                 JadeBowx.countCoeWifi()
                 print('CoE WiFi')
-                time.sleep(1)
+                
 
             if 'PSU WiFi 802.1x' in line:
                 JadeBowx.count802()
                 print('802.1x')
-                time.sleep(1)
+                
 
             if 'PSU WiFi 5GHz' in line:
                 JadeBowx.countPSU5Ghz()
                 print('PSU 5ghz')
-                time.sleep(1)
+                
 
             if 'TrueMove H' in line:
                 JadeBowx.countTruemove()
                 print('truemove')
-                time.sleep(1)
-
+            
             if  'CoEIoT' in line:
                 JadeBowx.countCoeIot()
                 print('coeiot')
-                time.sleep(1)
-
+            
             for patterns in rogue_list:
                 if re.search(patterns, line):
                     JadeBowx.countRogue()
                     print('others')
-                    time.sleep(1)
-
-            #Ap event
-            #floor 01
+            
+            #Ap Event
+            ##floor 01
             if 'AP3-46-R010-146' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP3-46-R010-146","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP218-FL01-E' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP218-FL01-E","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP217-FL01-W' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP217-FL01-W","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP204-R100' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP204-R100","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP216-R101' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP216-R101","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP211-Shop' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP211-Shop","floor":"01"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
 
             #floor 02
             if 'AP2-7-R020-153' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP2-7-R020-153","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP2-8-R020-154' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP2-8-R020-154","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP213-R202' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP213-R202","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP214-R203' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP214-R203","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP206-R204' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP206-R204","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP205-R207' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP205-R207","floor":"02"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
 
             #floor03
             if 'AP108-R300' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP108-R300","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP209-R302-1' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP209-R302-1","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP210-R302-2' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP210-R302-2","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP110-R311' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP110-R311","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP221-R301A' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP221-R301A","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP220-R301B' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP220-R301B","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP219-R303' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP219-R303","floor":"03"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
 
             #floor4
             if 'AP112-R400' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP112-R400","floor":"04"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP109-R404' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP109-R404","floor":"04"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP212-IDL' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP212-IDL","floor":"04"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP111-R405' in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP111-R405","floor":"04"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
             if 'AP215-R409'in line:
                 json_body = [{"measurement":"ap_event","tags":{"name":"AP215-R409","floor":"04"},"fields":{"item": 1}}]
                 dbClient.write_points(json_body)
-                time.sleep(1)
+                
 
         except EOFError:
-            running = False
+            break
+
     output.close()
 
 if __name__ == '__main__':
