f9f095ba754505c29e7933c35681ef9d740a3510
sabaszx <sabaszx@5910110573.psu.ac.th>
2020-03-13 19:50:47 +0700

    eiei

--- readLog.py
+++ readLog.py
@@ -2,7 +2,7 @@ import datetime
 import time
 import re
 from influxdb import InfluxDBClient
-
+import Filterx
 #loggt file date
 now = datetime.datetime.now()
 strnow = now.strftime("%X") #current time
@@ -11,38 +11,52 @@ dbClient = InfluxDBClient('localhost', 8086, 'sabaszx', 'admin', 'trapEvent', ss
 dbClient.switch_database('trapEvent')
 
 fileDate = now.strftime("%d-%b-%Y")
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces :
+        # Check if string is in the main string
+        if elem in mainString :
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    return  mainString
 
-def readUPS():
-    with open('/var/log/client_logs/172.30.254.201/UPS.log',"r") as clientLog:
-        while True:
-            try:
-                json_body = [{
-                    "measurement": "ssid_count",
-                    "tags": {
-                        "SSIDName": "unknown",
-                    "type": "others"},
-                    "fields": {
-                        "item": 1}
-                        }
-                    ]
-                dbClient.write_points(json_body)
-            except EOFError:
-                break
-        print(clientLog.read())
-    clientLog.close()
+
+#def readUPS():
+#    with open('/var/log/client_logs/172.30.254.201/UPS.log',"r") as clientLog:
+#        while True:
+#            except EOFError:
+#                break
+#        print(clientLog.read())
+#    clientLog.close()
 
 def testRead():
     with open('/var/log/client_logs/syslog/snmptrapd.log','r') as test:
         while True:
             try:
-                line = test.read()
-                if not line:
+                input_raw = test.read()
+                if not input_raw:
                     time.sleep(1)
-                print(line + '\n')
+
+                filtered = input_raw.replace("<UNKNOWN>","" )
+                showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
+
+                #filter out lookoup from Filterx  module 
+                wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
+                timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
+                hideMIB = replaceMultiple(timestamp, Filterx.mibList, '')
+                event = hideMIB.replace("snmpTrapOID","Event")
+                prefixRemove = replaceMultiple(event, Filterx.prefixList, '')
+                weirdRemove = replaceMultiple(prefixRemove, Filterx.weirdList, ' ')
+                bad_chars = "/\\!$^&*|'({)[}>_<]~+=#$%;`@?"
+
+                outstr  = replaceMultiple(weirdRemove,bad_chars,'')
+                #outstr - write log files into local server
+                result = replaceMultiple(outstr,Filterx.bad_list,' ')
+                print(result + '\n')
             except EOFError:
                 break
     test.close()
 
 if __name__ == '__main__':
-#    testRead()
-    readUPS()
+    testRead()
+#    readUPS()
