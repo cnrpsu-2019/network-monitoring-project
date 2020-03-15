066284bde41bd1d004268b932dcdcd126b7cc4a5
sabaszx <5910110573@psu.ac.th>
2020-03-15 23:49:20 +0700

    to edit in windows

---
+++ test.py
@@ -0,0 +1,17 @@
+import re
+
+def main():
+    readTest = open('example.log','r')
+    pattern = re.compile(r'(?:[0-9a-fA-F]:?){12}') #compile mac address pattern xx:xx:xx:xx:xx:xx
+    txt = readTest.read() #raw txt files
+
+    resultMac = re.findall(pattern, txt) #now there's only list of mac addresses
+    # print(resultMac)
+    readTest.close()
+
+    
+
+if __name__ == "__main__":
+    main() 
+    
+#add mac address to regcognize use
