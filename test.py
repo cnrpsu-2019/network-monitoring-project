b121c0dabb9c85e7dff342971ef6ed73aa557911
sabaszx <sabaszx@5910110573.psu.ac.th>
2020-03-18 19:30:01 +0700

    to edit in client side

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
 import re
+import datetime
+import time
+
+now = datetime.datetime.now()
+strnow = now.strftime("%X") #current time
+#log file date
+path = '/home/bass/receive/'
+fileDate = now.strftime("%d-%b-%Y")
+fileName = path + 'trapd-' + fileDate + '.log'
+sample = 'example.log'
 
 def main():
-    readTest = open('example.log','r')
+    readTest = open(fileName,'r')
     pattern = re.compile(r'(?:[0-9a-fA-F]:?){12}') #compile mac address pattern xx:xx:xx:xx:xx:xx
     txt = readTest.read() #raw txt files
-
-    resultMac = re.findall(pattern, txt) #now there's only list of mac addresses
-    print(resultMac)
-    #close file
-    readTest.close()
+    while True:
+        try:
+            resultMac = re.findall(pattern, txt) #now there's only list of mac addresses
+            if not resultMac:
+                time.sleep(1)
+            print(resultMac)
+        except EOFError:
+            break
+        finally:
+            #close file
+            readTest.close()
 if __name__ == "__main__":
     main() 
     
