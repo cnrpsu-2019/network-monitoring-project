ef1d7693e731aa03b2b3b69cfeaba3ae7d9a39d3
sabaszx <sabaszx@5910110573.psu.ac.th>
2020-04-04 16:56:18 +0700

    to edit

--- backup.py
+++ backup.py
@@ -4,7 +4,7 @@ import string
 import sys
 import Filterx
 import time
-from influxdb import InfluxDBClient
+#from influxdb import InfluxDBClient
 
 def replaceMultiple(mainString, toBeReplaces, newString):
     # Iterate over the strings to be replaced
@@ -25,19 +25,19 @@ def main():
     fileName = "trapd-" + fileDate + ".log"
     output = open('/home/bass/receive/' + fileName, 'a')
     readfile = open('/home/bass/receive/' + fileName, 'r')
-
-    dbClient = InfluxDBClient('localhost', 8086, 'sabaszx', 'admin', 'trapEvent', ssl=False, verify_ssl=False)
-    dbClient.create_database('trapEvent')
+#
+#    dbClient = InfluxDBClient('localhost', 8086, 'sabaszx', 'admin', 'trapEvent', ssl=False, verify_ssl=False)
+#    dbClient.create_database('trapEvent')
 
     #print('Database created, go check in shell')
 
-    dbClient.switch_database('trapEvent')
+#    dbClient.switch_database('trapEvent')
     known_ssid_list = ["PSU WiFi 802.1x","PSU WiFi 5GHz","TrueMove H","CoEIoT","CoEWiFi"]
 
     while running:
         try:
-            input = raw_input()
-            filtered = input.replace("<UNKNOWN>","" )
+            raw_input = input()
+            filtered = raw_input.replace("<UNKNOWN>","" )
             showDate = filtered.replace("UDP: [172.30.232.2]:32768->[172.30.232.250]:162", strnow)
             wrongtypeRemove = replaceMultiple(showDate, Filterx.wronglist, '')
             timestamp = wrongtypeRemove.replace("DISMAN-EVENT-MIB::", "")
