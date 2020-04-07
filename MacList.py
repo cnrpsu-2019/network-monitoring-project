81d35c5ae703d08ec1c2475a7047d9f436e8af84
sabaszx <sabaszx@5910110573.psu.ac.th>
2020-04-07 23:01:29 +0700

    eyey

---
+++ MacList.py
@@ -0,0 +1,16 @@
+#lists of ap mac address each floor
+floor01_mac_list = ['bc:16:f5:98:8:0','68:3b:78:e1:8e:c0','68:3b:78:e1:94:20','f4:4e:5:a2:a1:d0',
+                    'dc:8c:37:4c:2e:e0','f4:4e:5:b5:24:b0']
+
+floor02_mac_list = ['88:1d:fc:a:f1:20','88:1d:fc:6:3f:b0','70:10:5c:b1:a4:d0','a0:e0:af:3d:c7:80',
+                   'f4:4e:5:a2:a1:10','f4:4e:5:b5:65:90']
+
+floor03_mac_list = ['a0:3d:6f:31:b7:e0','f4:4e:5:df:4e:f0','f4:4e:5:db:f0:40','70:10:5c:b1:9e:d0',
+                    '68:3b:78:e1:93:0','68:3b:78:e1:93:80','68:3b:78:e9:47:40']
+
+floor04_mac_list = ['a0:e0:af:22:6e:70','a0:3d:6f:31:b7:f0','a0:3d:6f:31:b7:d0','a0:e0:af:95:9c:40',
+                   'a0:3d:6f:dd:2d:70']
+
+#all ap mac addresses in building
+
+buildingMacList = floor01_mac_list + floor02_mac_list + floor03_mac_list + floor04_mac_list
