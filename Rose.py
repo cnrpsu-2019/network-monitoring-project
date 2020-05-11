211a8a1a86bbc34c7cf9b40dc831efc99ea567ce
sabaszx <5910110573@psu.ac.th>
2020-05-12 00:27:28 +0700

    fix txt

--- Rose.py
+++ Rose.py
@@ -3,19 +3,7 @@ import Extract
 import createFiles
 import time
 import ExportToDB
-'''
-เอาล่ะ มาเริ่มกันใหม่ เนื่องจาก session id มันค่อนค่างแตกต่างนะ
-เอา mac addr สองตัว มาเทียบกัน
-ถ้าเหมือนกัน : ยอดเท่าเดิม
-ไม่เหมือนกัน : ดูสถานะ
-	assocaited : total_user++
-	disassocicate : total_user--
-จากนั้น : ยัดลง database
-เคลียร์สถานะ
 
-ดีเลย์อีก 5 นาที ค่อยนับใหม่
-Associate Disassociate
-'''
 def harvest_user():
 	status = Extract.extractSpecific(createFiles)
 	prev_mac = Extract.client_mac(createFiles.realFile)[-1]
@@ -28,9 +16,9 @@ def harvest_user():
 			if curr_mac == prev_mac:
 				time.sleep(5) #snooze for 5 secs
 			elif curr_mac != prev_mac:
-				if status is Associate:
+				if status is 'Associate':
 					total_user = total_user + 1
-				elif status is Disassociate:
+				elif status is 'Disassociate':
 					total_user = total_user - 1
 
 			ExportToDB.harvest_user(total_user)
