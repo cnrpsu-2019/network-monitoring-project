41737b827dc2f36abd5be979efe3652fcee36652
sabaszx <5910110573@psu.ac.th>
2020-05-13 16:58:09 +0700

    edit logic

--- Rose.py
+++ Rose.py
@@ -3,20 +3,12 @@ import Extract
 import createFiles
 import time
 import ExportToDB
+import datetime
 
 def harvest_user(): #collect user by mac address
 	status = Extract.extractSpecific(createFiles.realFile,'Event').split('Event')[-1].strip()
 	curr_mac = Extract.client_mac(createFiles.realFile)[-1].strip()
-	prev_mac = Extract.client_mac(createFiles.realFile)[-2].strip()
 	bucket = []
-	total_user = 0
-	
-	if curr_mac is not prev_mac:
-		if status is 'Associate':
-			bucket.append(curr_mac)
-		elif status is 'Disassociate':
-			if bucket is not []:
-				bucket.pop()
-	
+	bucket.append(curr_mac)
 	total_user = int(len(bucket))
 	ExportToDB.harvest_user(total_user)
