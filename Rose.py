a795acdcac6b2efee6eb3bcec6915767439c88e2
sabaszx <5910110573@psu.ac.th>
2020-05-14 19:49:01 +0700

    time scheduler

--- Rose.py
+++ Rose.py
@@ -3,12 +3,18 @@ import Extract
 import createFiles
 import time
 import ExportToDB
-import datetime
+import schedule
 
 def harvest_user(): #collect user by mac address
 	status = Extract.extractSpecific(createFiles.realFile,'Event').split('Event')[-1].strip()
-	curr_mac = Extract.client_mac(createFiles.realFile)[-1].strip()
+	curr_mac = Extract.client_mac(createFiles.realFile) #collect all mac addresses
 	bucket = []
 	bucket.append(curr_mac)
 	total_user = int(len(bucket))
 	ExportToDB.harvest_user(total_user)
+
+schedule.every(5).minutes.do(harvest_user)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
