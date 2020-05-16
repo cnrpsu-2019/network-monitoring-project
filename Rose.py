374b38e499d7bf5a734823348642b814a72dc624
sabaszx <5910110573@psu.ac.th>
2020-05-17 01:12:33 +0700

    fix collect all users

--- Rose.py
+++ Rose.py
@@ -7,7 +7,7 @@ import ExportToDB
 def harvest_user(): #collect user by mac address
     status = Extract.extractSpecific(createFiles.realFile,'Event').split('Event')[-1].strip()
     curr_mac = Extract.client_mac(createFiles.realFile) #collect all mac addresses 
-    bucket = []
-    bucket.append(curr_mac)
-    total_user = int(len(bucket))
+    # bucket = []
+    # bucket.append(curr_mac)
+    total_user = int(len(curr_mac))
     ExportToDB.harvest_user(total_user)
