0451d97e4e1edaf5021657f42dbef86e05019c79
sabaszx <5910110573@psu.ac.th>
2020-05-17 00:30:55 +0700

    get all of unique users

--- Rose.py
+++ Rose.py
@@ -6,7 +6,7 @@ import ExportToDB
 
 def harvest_user(): #collect user by mac address
     status = Extract.extractSpecific(createFiles.realFile,'Event').split('Event')[-1].strip()
-    curr_mac = Extract.client_mac(createFiles.realFile) #collect all mac addresses
+    curr_mac = Extract.client_mac(createFiles.realFile) #collect all mac addresses 
     bucket = []
     bucket.append(curr_mac)
     total_user = int(len(bucket))
