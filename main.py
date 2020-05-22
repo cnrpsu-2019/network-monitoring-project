6bbf02665d54aadfc2491a82f8c7e87deebfe090
sabaszx <5910110573@psu.ac.th>
2020-05-23 01:28:03 +0700

    edit syntax

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def main():
             #replace string
             result = filterString.filter_string(raw_input)
             #write to local server
-            if result is 'UDP: 172.30.232.2:32768-172.30.232.250:162': #coe wlc
+            if result == 'UDP: 172.30.232.2:32768-172.30.232.250:162': #coe wlc
                 ExportToDB.uptime_wlc('172.30.232.2 - CoE',B612.uptime_instance)
-            elif result is 'UDP: 172.31.253.2:32769-172.30.232.250:162': #eng psu
+            elif result == 'UDP: 172.31.253.2:32769-172.30.232.250:162': #eng psu
                 ExportToDB.uptime_wlc('172.30.253.2 - EnG',B612.uptime_instance)
 
             output.write(result +'\n')
